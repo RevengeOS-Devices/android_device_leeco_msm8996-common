5c1115948db09b0c7197f1c91082ae2682247d31
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-19 08:32:12 -0700

    msm8996-common: Move hardware specific .rc files to /vendor/etc/init/hw
    also move from root to etc
    The reason these files are moved to /vendor/etc/init/hw and
    not /vendor/etc/init is because 'init' scans the /vendor/etc/init folder
    for vendor service .rc fragments to import. That would either result
    in duplicate imports or out-of-order imports of these hardware
    specific .rc files and that *may* have unintended consequences.
    
    In order to preserve the current import order for MTP, we move all
    hardware specific .rc files to /vendor/etc/init/hw which is not
    autoscanned by init BUT leave the init.${ro.hardware}.rc (i.e.
    init.qcom.rc in this case) in root for 'init' to find.
    
    Change-Id: I14531da05e2a3a256f734f8de96e1f1969c753f9

---
+++ rootdir/Android.mk
@@ -0,0 +1,72 @@
+LOCAL_PATH := $(call my-dir)
+
+# Device init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.qcom
+LOCAL_VENDOR_MODULE	:= true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qcom.power.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.qcom.power.rc
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qcom.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.qcom.rc
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qcom.usb.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.qcom.usb.rc
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qcom.usb.sh
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= EXECUTABLES
+LOCAL_SRC_FILES		:= etc/init.qcom.usb.sh
+LOCAL_VENDOR_MODULE	:= true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qcom.bt.sh
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= EXECUTABLES
+LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
+LOCAL_VENDOR_MODULE	:= true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= ueventd.qcom.rc
+LOCAL_MODULE_STEM	:= ueventd.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
+
+LOCAL_MODULE		:= init.qcom.sh
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= EXECUTABLES
+LOCAL_SRC_FILES		:= etc/init.qcom.sh
+LOCAL_VENDOR_MODULE	:= true
+include $(BUILD_PREBUILT)
+
+
