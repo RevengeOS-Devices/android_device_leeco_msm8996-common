b1e536bb0d28c97b51080886834e3fbb3945dba6
Ethan Chen <intervigil@gmail.com>
2017-09-17 13:14:00 +0800

    msm8996: Add basic USB HAL that reports no status change

--- msm8996.mk
+++ msm8996.mk
@@ -354,7 +354,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
+    android.hardware.usb@1.0-service.msm8996 \
     com.android.future.usb.accessory
 
 # Vibrator
