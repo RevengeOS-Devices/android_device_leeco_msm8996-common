f7a3fc08c2e3ff623a578d5da3f52fc64deaab02
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    msm8996-common: power: switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- msm8996.mk
+++ msm8996.mk
@@ -284,7 +284,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service.gemini
+    android.hardware.power@1.2-service.leeco-libperfmgr
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
