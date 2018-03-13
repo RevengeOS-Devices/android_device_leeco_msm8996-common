be9426980593d68e66477982ef3e676d1c08b297
DennySPB <dennyspb@gmail.com>
2018-03-13 11:36:30 +0300

    EAS: BoardConfig: set flag TARGET_USES_AOSP for Power Hal

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,6 +199,7 @@ TARGET_USES_MKE2FS := true
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_USES_AOSP := true
 
 # Recovery
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_leeco
