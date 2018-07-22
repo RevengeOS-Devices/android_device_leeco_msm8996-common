daaddbeb3c43b8d5a0e97696c2115f91182679e5
Lucchetto <lucchetto.tie@live.com>
2018-07-22 16:51:35 +0200

    Use GoogleCamera
    Change-Id: Ib81ed0dc28c5cdf8fca61f0bb5cc4ea8977d6d31

--- msm8996.mk
+++ msm8996.mk
@@ -166,7 +166,7 @@ PRODUCT_PACKAGES += \
     camera.msm8996 \
     libshims_camera \
     libshims_qcamera-daemon \
-    Snap
+    GoogleCamera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
