414508700aac23460141ad41a09c1a405b9aa6f1
Lucchetto <lucchetto.tie@live.com>
2018-08-20 10:57:20 +0200

    Remove livedisplay

--- msm8996.mk
+++ msm8996.mk
@@ -260,10 +260,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8996
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
