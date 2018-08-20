0abeb1bb7612c89d02f78f486467ed0908f42c8c
Lucchetto <lucchetto.tie@live.com>
2018-08-21 00:22:19 +0200

    Nuke recovery

--- msm8996.mk
+++ msm8996.mk
@@ -317,10 +317,6 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_leeco
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
