53712f7118eb5f70c8e670ceb6135745e9d756e2
LuK1337 <priv.luk@gmail.com>
2018-09-07 15:21:20 +0200

    msm8996-common: Drop libbt-vendor
    * With QTI bluetooth it's no longer needed.
    
    Change-Id: Iba4b0b6ba24fe1b85e7175291ed84769615ffadd

--- msm8996.mk
+++ msm8996.mk
@@ -124,10 +124,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
