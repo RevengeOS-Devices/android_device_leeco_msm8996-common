95d33601cdb50094ebc7903caacc9b933a0af8c5
shivatejapeddi <shivatejapeddi@gmail.com>
2018-09-11 00:16:24 +0530

    msm8996-common : bring Healthd inline with caf

--- msm8996.mk
+++ msm8996.mk
@@ -147,9 +147,9 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service \
+    libhealthd.msm
 
 # Display
 PRODUCT_PACKAGES += \
