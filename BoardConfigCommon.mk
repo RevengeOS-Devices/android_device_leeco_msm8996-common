LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Move AtFwd hal from device to framework manifest.
AtFwd hal is implemented as an android service and
it stars from system partition, that is, it's not
a traditional vendor hal and hence it should be
declared in the framework manifest and not in the
device manifest.
This commit introduces new device specific framework
manifest (framework_manifest.xml) and moves AtFwd hal
from device to framework manifest.

Remove IConfig and IOemHook hals.

Change-Id: Iacdb5604a682b8462d16297f4b543928bb100282
CRs-Fixed: 2287149

BoardConfigCommon.mk
@@ -137,6 +137,7 @@ TARGET_EXFAT_DRIVER := sdfat
 TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 
 # HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 

RevengeOS-Devices/android_device_leeco_msm8996-common

BoardConfigCommon.mk
@@ -187,6 +187,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
 # HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(PLATFORM_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
 