26c8c4270ef06125dc157b8edb28df547b96f124
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-11 05:37:55 +0000

    msm8996 : Enable sound trigger

--- msm8996.mk
+++ msm8996.mk
@@ -83,6 +83,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
