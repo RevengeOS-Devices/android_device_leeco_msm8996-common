1b988203ec77340f4bc71cfb12262ae6fde2fe5c
Lucchetto <lucchetto.tie@live.com>
2018-10-27 13:49:17 +0200

    Changes for CAF

--- msm8996.mk
+++ msm8996.mk
@@ -336,6 +336,13 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
