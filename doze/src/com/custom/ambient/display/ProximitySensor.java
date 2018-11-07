5ab0069116a66216b5195657f883d0d69ada73ab
shivatejapeddi <shivatejapeddi@gmail.com>
2018-11-07 08:07:19 +0000

    msm8996 : Add Custom Doze Package

--- doze/src/com/custom/ambient/display/ProximitySensor.java
+++ doze/src/com/custom/ambient/display/ProximitySensor.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2015 The CyanogenMod Project
- *               2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.doze;
+package com.custom.ambient.display;
 
 import android.content.Context;
 import android.hardware.Sensor;
@@ -33,29 +32,25 @@ public class ProximitySensor implements SensorEventListener {
     private static final boolean DEBUG = false;
     private static final String TAG = "ProximitySensor";
 
-    // Maximum time for the hand to cover the sensor: 1s
-    private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
-
-    // Minimum time until the device is considered to have been in the pocket: 2s
-    private static final int POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000;
+    private static final int POCKET_DELTA_NS = 1000 * 1000 * 1000;
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private Context mContext;
-    private ExecutorService mExecutorService;
 
     private boolean mSawNear = false;
     private long mInPocketTime = 0;
 
+    private final ExecutorService mExecutorService;
+
     public ProximitySensor(Context context) {
         mContext = context;
-        mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mSensorManager = (SensorManager)
+                mContext.getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager != null) {
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        }
         mExecutorService = Executors.newSingleThreadExecutor();
-    }
-
-    private Future<?> submit(Runnable runnable) {
-        return mExecutorService.submit(runnable);
     }
 
     @Override
@@ -71,15 +66,22 @@ public class ProximitySensor implements SensorEventListener {
         mSawNear = isNear;
     }
 
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
     private boolean shouldPulse(long timestamp) {
         long delta = timestamp - mInPocketTime;
 
-        if (Utils.isHandwaveGestureEnabled(mContext) && Utils.isPocketGestureEnabled(mContext)) {
+        if (Utils.handwaveGestureEnabled(mContext)
+                    && Utils.pocketGestureEnabled(mContext)) {
             return true;
-        } else if (Utils.isHandwaveGestureEnabled(mContext)) {
-            return delta < HANDWAVE_MAX_DELTA_NS;
-        } else if (Utils.isPocketGestureEnabled(mContext)) {
-            return delta >= POCKET_MIN_DELTA_NS;
+        } else if (Utils.handwaveGestureEnabled(mContext)
+                    && !Utils.pocketGestureEnabled(mContext)) {
+            return delta < POCKET_DELTA_NS;
+        } else if (!Utils.handwaveGestureEnabled(mContext)
+                    && Utils.pocketGestureEnabled(mContext)) {
+            return delta >= POCKET_DELTA_NS;
         }
         return false;
     }
--- msm8996.mk
+++ msm8996.mk
@@ -171,7 +171,7 @@ PRODUCT_PACKAGES += \
 
 # Doze mode
 PRODUCT_PACKAGES += \
-    Doze
+    LeecoDoze
 
 # DRM
 PRODUCT_PACKAGES += \
