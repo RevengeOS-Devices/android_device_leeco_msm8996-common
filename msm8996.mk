6795f7ea423a6e3682e99ab856d343e17b020eda
Lucchetto <lucchetto.tie@live.com>
2019-02-05 20:15:32 +0100

    Revert "MSM8996: Move Audio HAL from 2.0 to 4.0"
    This reverts commit 400093c040ee133722e8720f76f27428c19e2c4f.

--- msm8996.mk
+++ msm8996.mk
@@ -80,9 +80,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
