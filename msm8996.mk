58ab06ea576b5c5182e1c4a2eb6e3f5048fac13c
Lucchetto <lucchetto.tie@live.com>
2019-06-04 10:14:16 +0200

    msm8996-common: remove powerhint.xml
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- msm8996.mk
+++ msm8996.mk
@@ -288,9 +288,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service.gemini
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Qualcomm
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
