35ad27db5e927e7f8c3a0e695b72d890403230ff
Lucchetto <lucchetto.tie@live.com>
2019-09-05 09:27:14 +0200

    msm8996-common: drop boot jars
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- msm8996.mk
+++ msm8996.mk
@@ -332,9 +332,6 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
@@ -409,9 +406,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
 
 # Model is set via init library
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
