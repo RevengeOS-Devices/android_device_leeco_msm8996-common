showp1874/android_device_zuk_msm8996-common

msm8996-common: Enable system-as-root
Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
Change-Id: Idd76dec60c9a7a010372b28b29c7ff9aa1da08e0
Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

msm8996.mk
@@ -156,6 +156,11 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     ueventd.qcom.rc
 
+# Device mapper verity
+PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/factory
+$(call inherit-product, build/target/product/verity.mk)
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \

RevengeOS-Devices/android_device_leeco_msm8996-common

msm8996.mk
@@ -136,6 +136,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
 
+# Device mapper verity
+PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/vendor
+$(call inherit-product, build/target/product/verity.mk)
+
 # Device settings
 PRODUCT_PACKAGES += \
     DeviceSettings