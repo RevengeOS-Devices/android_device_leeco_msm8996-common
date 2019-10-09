27e567ef7d651f66f0917c64bea4e61d584d85e1
dianlujitao <dianlujitao@lineageos.org>
2019-10-09 19:54:50 +0800

    msm8996-common: Build new GNSS HAL
     * Move msm8996 to GNSS HIDL 2.0 to be compatible with msm8953 blobs
     * Remove unnecessary dependencies on liblbs_core.so
     * Set QC valueadd props and hardcode vendor loc HIDL version to 3.0 to
       enable vendor HIDL and enhanced framework features.
    
    Change-Id: I01f661f1d1e038737801f5a9db88e3388d470f74

--- msm8996.mk
+++ msm8996.mk
@@ -192,11 +192,14 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    libcurl \
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti \
+    libbatching \
+    libgeofencing \
     libgnss \
     libgnsspps \
-    libsensorndkbridge
+    libsensorndkbridge \
+    libwifi-hal-ctrl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
