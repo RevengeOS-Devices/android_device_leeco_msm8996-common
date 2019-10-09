c72dceadf9c69c0cb1a4a1c9e87856ca42f0fd54
Lucchetto <lucchetto.tie@live.com>
2019-10-09 17:48:40 +0200

    Revert "msm8996-common: drop boot jars"
    This reverts commit 35ad27db5e927e7f8c3a0e695b72d890403230ff.
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- msm8996.mk
+++ msm8996.mk
@@ -329,6 +329,9 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     telephony-ext
 
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
@@ -407,6 +410,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
 
 # Model is set via init library
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
