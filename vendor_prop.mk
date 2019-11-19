0f2ca718d5ebca8a3f56244e60f8080a4cecf98a
dianlujitao <dianlujitao@lineageos.org>
2019-11-19 19:33:47 +0800

    msm8996-common: Disable UBWC for WFD
     * vendor.gralloc.disable_wb_ubwc is removed from Q blobs and replaced
       by this one.
    
    Change-Id: I7c1457ccff4a142ad3a5b83ac11ffe333303667a

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -106,7 +106,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_rotator_split=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
-    vendor.display.perf_hint_window=50
+    vendor.display.perf_hint_window=50 \
+    vendor.video.disable.ubwc=1
 
 # Graphics (OpenGLES)
 PRODUCT_PROPERTY_OVERRIDES += \
