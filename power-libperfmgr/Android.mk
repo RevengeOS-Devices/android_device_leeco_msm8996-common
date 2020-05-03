SyberiaProject-Devices/platform_device_xiaomi_gemini

power-libperfmgr: convert .bp to .mk
this needed for adding custom flags without soong changes

Change-Id: Ic571f7aa0a01940f0ea1d018a2ec4202b7291a1b
Signed-off-by: DennySPB <dennyspb@gmail.com>

power-libperfmgr/Android.mk
@@ -1,0 +1,42 @@
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := qcom
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := android.hardware.power@1.2-service.gemini-libperfmgr
+LOCAL_INIT_RC := android.hardware.power@1.2-service.gemini-libperfmgr.rc
+LOCAL_SRC_FILES := service.cpp Power.cpp InteractionHandler.cpp power-helper.c
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libcutils \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libutils \
+    android.hardware.power@1.0 \
+    android.hardware.power@1.1 \
+    android.hardware.power@1.2 \
+    libperfmgr
+
+include $(BUILD_EXECUTABLE)
+endif

RevengeOS-Devices/android_device_leeco_msm8996-common

power-libperfmgr/Android.mk
@@ -1,0 +1,42 @@
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := qcom
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := android.hardware.power@1.2-service.leeco-libperfmgr
+LOCAL_INIT_RC := android.hardware.power@1.2-service.leeco-libperfmgr.rc
+LOCAL_SRC_FILES := service.cpp Power.cpp InteractionHandler.cpp power-helper.c
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libcutils \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libutils \
+    android.hardware.power@1.0 \
+    android.hardware.power@1.1 \
+    android.hardware.power@1.2 \
+    libperfmgr
+
+include $(BUILD_EXECUTABLE)
+endif