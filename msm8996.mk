aff2161c5ce4b6a2d2367e5e485335d7cf26b931
Lucchetto <lucchetto.tie@live.com>
2020-09-13 09:37:48 +0200

    msm8996-common: build libhwbinder
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- msm8996.mk
+++ msm8996.mk
@@ -125,6 +125,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
