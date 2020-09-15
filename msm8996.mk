d0861cae6bf7430a3212d7f74d3b9ce4ab5e8b05
Lucchetto <lucchetto.tie@live.com>
2020-09-15 09:38:10 +0200

    msm8996-common: build libhidltransport
        * Required by some blobs
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- msm8996.mk
+++ msm8996.mk
@@ -223,7 +223,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor
 
 # IPA Manager
 PRODUCT_PACKAGES += \
